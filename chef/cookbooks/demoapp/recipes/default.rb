ed14ce4923305f79770d722a74210bb1e09bc827
Josh Pasqualetto <josh.pasqualetto@gmail.com>
2016-04-24 14:40:02 -0600

    [chef] Demoapp deploy

--- chef/cookbooks/demoapp/recipes/default.rb
+++ chef/cookbooks/demoapp/recipes/default.rb
@@ -7,4 +7,37 @@
 # All rights reserved - Do Not Redistribute
 #
 
-file "/tmp/testing"
+package 'ruby1.9.3'
+package 'bundler'
+
+group node[:demoapp][:user]
+
+user node[:demoapp][:user] do
+  home node[:demoapp][:home]
+  group node[:demoapp][:user] 
+  supports :manage_home => true
+  system true
+  shell '/bin/bash'
+end
+
+deploy_revision node[:demoapp][:home] do
+  revision node[:demoapp][:revision]
+  user node[:demoapp][:user]
+  repository node[:demoapp][:repo]
+  symlinks Hash.new
+  purge_before_symlink Array.new
+  create_dirs_before_symlink Array.new
+  symlink_before_migrate Hash.new
+  migrate false
+
+  before_migrate do
+    execute 'Run Bundler' do
+      command 'bundle install'
+      cwd release_path
+      user node[:demoapp][:user]
+    end
+  end
+
+  restart do
+  end
+end
